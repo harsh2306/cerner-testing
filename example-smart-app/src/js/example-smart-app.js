ce89b73e3309800a11cd55bce03d68160621f4f9
harsh2306 <harshu2306shah69@gmail.com>
2022-07-02 18:08:51 +0530

    initial commit 2

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -28,6 +28,7 @@
         $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
+
           var fname = '';
           var lname = '';
 
@@ -35,7 +36,6 @@
             fname = patient.name[0].given.join(' ');
             lname = patient.name[0].family.join(' ');
           }
-          
 
           var height = byCodes('8302-2');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
@@ -49,60 +49,7 @@
           p.fname = fname;
           p.lname = lname;
           p.height = getQuantityValueAndUnit(height[0]);
-          p.id = patient.id;
-          
-          
-          let getMRN = patient.identifier;
 
-          // for(i=0 ; i <= getMRN.length ; i++) {
-          //   getText = i.find('text')
-          //   if (getText != null && getText['value'] == "EPI"){
-          //     mrnValue = i.find('value')
-
-          //     patientMRN = mrnValue['value']
-          //   }
-          // }
-          // p.mrn = patientMRN; 
-
-          // console.log("printing mrn after loop" , p.mrn);
-          // Object.entries(getMRN).forEach(([key, value]) => {
-          //   if(key == 1){
-          //     console.log("printing from inside");
-          //     console.log(value[value]);
-          //   }
-          // });
-          // var obj = JSON.parse(getMRN);
-
-          // // Define recursive function to print nested values
-          // function printValues(obj) {
-          //     for(var k in obj) {
-          //         if(obj[k] instanceof Object) {
-          //             printValues(obj[k]);
-          //         } else {
-          //             document.write(obj[k] + "<br>");
-          //         };
-          //     }
-          // };
-
-          console.log("printing get mrn", getMRN);
-          let obj = getMRN[1];
-          console.log("printing obj" , obj);
-          let obj2 = JSON.parse(obj);
-          console.log("printing obj2" , obj2);
-          // let split1 = String(obj2);
-          // console.log('split 1: ' + split1);
-          // let split2 = String(split1.split(","));
-          // console.log("printing split2", split2[1]);
-          // let split3 = String(split2[1].split(":"));
-          // console.log('printing split 3', split3[1]);
-
-          // console.log("helo", getMRN[1][value]);
-
-          let tempvar = patient.identifier[1]; 
-          console.log("printing patient: ", patient); 
-          console.log("printing getMRn: ", getMRN); 
-          // console.log("printing tempvar[1]: ", tempvar);
-          // console.log("printing getmrn value: ", getMRN[1].join(' '));
           if (typeof systolicbp != 'undefined')  {
             p.systolicbp = systolicbp;
           }
@@ -137,8 +84,6 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
-      id: {value: ''},
-      mrn: {value: ''},
     };
   }
 
@@ -171,7 +116,7 @@
   }
 
   window.drawVisualization = function(p) {
-    $('#holder').hide();
+    $('#holder').show();
     $('#loading').hide();
     $('#fname').html(p.fname);
     $('#lname').html(p.lname);
@@ -182,8 +127,6 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
-    $('#id').html(p.id);
-    $('#mrn').html(p.mrn);
   };
 
 })(window);
